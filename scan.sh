e24933ddd48d41ba4e1191ee3cc954276b05fdf3
hanzel_sc <hanselkansam04@gmail.com>
2025-05-07 14:38:37 +0530

    Configured report formats to text files

--- scan.sh
+++ scan.sh
@@ -2,11 +2,13 @@
 
 mkdir -p reports
 
+timestamp=$(date +"%Y%m%d%H%M%S")
+
 # Running the terrascan scan
-/usr/local/bin/terrascan scan -t aws -d . -o text > reports/terrascan_report.txt
+/usr/local/bin/terrascan scan -t aws -d . -o json > reports/terrascan_report_$timestamp.json
 
 #Running the TFsec scan
-/usr/local/bin/tfsec . --no-cache --format text > reports/tfsec_report.txt
+/usr/local/bin/tfsec . --no-cache --format text > reports/tfsec_report_$timestamp.txt
 
 echo "Terrascan and tfsec reports saved to $(pwd)/reports/"
 
